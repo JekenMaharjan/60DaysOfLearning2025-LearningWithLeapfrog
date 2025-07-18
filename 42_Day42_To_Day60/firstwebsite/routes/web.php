0f8b2c855d45d03557b4db714e8c4be26a20a120
Jeken Maharjan <maharjanjeken@gmail.com>
2025-07-18 19:01:42 +0545

    added day47 - How to use get type routes

--- 42_Day42_To_Day60/firstwebsite/routes/web.php
+++ 42_Day42_To_Day60/firstwebsite/routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Get route example
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Parameters using routes
+Route::get('/portfolio/{firstname}/{lastname}', function ($firstname, $lastname) {
+    return $firstname . " " . $lastname;
+});
+
