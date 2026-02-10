6b7d8d4d634c811d77a4f78b8bf9b0c351829151
JekenMaharjan <maharjanjeken@gmail.com>
2026-02-10 15:20:33 +0545

    Day 1: Add comments on index.php and change .txt file into .md file

--- 01_Day1/index.php
+++ 01_Day1/index.php
@@ -1,10 +1,21 @@
-<!DOCTYPE html>
-<html>
-<body>
- 
-<?php
-echo "My first PHP script!";
-?>
 
-</body>
+<!-- 
+-> Run on localhost: php -S localhost:8000
+-> Run on terminal: php index.php
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <?php
+            echo "My first PHP script!";
+        ?>
+    </body>
 </html>
+
+
